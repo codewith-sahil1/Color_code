666c6cd4e9db038038c01659deb9eedf59a43d19
sahil <shlrjsrivastava@gmail.com>
2024-08-23 19:24:23 +0530

    changing of the three background colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-
+import React, { useState } from "react"
 
 
 function App() {
 
-
+  const [color, setcolor] = useState("olive")
+  function fun() {
+    setcolor("red")
+  }
+  function fun2(){
+    setcolor("blue")
+  }
+  function fun3(){
+    setcolor("yellow")
+  }
   return (
-    <>
 
-    </>
+
+    <div style={{
+      backgroundColor: color, height: "100vh", width: "100vw", color: "white"
+    }}>
+      <button onClick={fun} style={{ height: "40px", width: "40px", alignItems: "center", backgroundColor: "red", color: "white" }}>red</button>
+
+      <button onClick={fun2}  style={{height:"50px", width:"50px", backgroundColor:"blue",color:"white"}}>Blue</button>
+      <button onClick={fun3} style={{height:"50px", width:"50px",backgroundColor:"yellow",color:"white"}}>yellow</button>
+    </div>
+
+
   )
 }
 
